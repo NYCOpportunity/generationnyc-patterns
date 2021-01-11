ab88ccfd6d2bdbbc2b8d387af30caba9f463144e
kimpenguin <kpeng@nycopportunity.nyc.gov>
2021-01-11 12:13:39 -0500

    adding temporary label update for pagination

--- src/utilities/pagination/pagination.js
+++ src/utilities/pagination/pagination.js
@@ -21,6 +21,8 @@ class Pagination {
       if (this.settings.hash) {
         const el = document.querySelector(`[href="${this.settings.hash}"]`)
         Pagination.toggle(el)
+      } else {
+        Pagination.toggle(triggers[0])
       }
 
     } else{
@@ -29,8 +31,10 @@ class Pagination {
   }
 }
 
+/**
+ * Toggles the active anchor and associated sections
+ */
 Pagination.toggle = function(element){
-
   // toggle active class on side navigation
   const children = Array.from(element.parentNode.children);
 
@@ -47,13 +51,44 @@ Pagination.toggle = function(element){
   children_sections.forEach(function (child) {
     if (child.tagName == 'SECTION'){
       child.classList.add('hidden')
+      child.classList.remove('active')
     }
   })
 
   active_section.classList.remove('hidden');
+  active_section.classList.add('active');
+
+  // update the text on buttons
+  // Pagination.updateLabel()
+  
+}
+
+/**
+ * Update the Previous and Next descriptions
+ */
+Pagination.updateLabel = function(){
+  // get the anchor link that is active
+  let container = document.querySelector(Pagination.anchor).parentNode;
+  let el = container.querySelectorAll('.active');
+
+  let prev = document.querySelector(Pagination.prev)
+  let next = document.querySelector(Pagination.next)
+  
+  if(el[0].previousElementSibling) {
+    prev.textContent = el[0].previousElementSibling.innerText.trim()
+    prev.parentElement.parentElement.href = el[0].previousElementSibling.href
+  }
+  
+  if(el[0].nextElementSibling) {
+    next.textContent = el[0].nextElementSibling.innerText.trim()
+    next.parentElement.parentElement.href = el[0].nextElementSibling.href
+  }
 }
 
 /** @param  {String}  selector  The main selector for the pattern */
-Pagination.trigger = '[data-trigger="paginate"]';
+Pagination.trigger = '[data-trigger*="paginate"]';
+Pagination.anchor = '[data-trigger*="paginate-anchor"]';
+Pagination.prev = '[data-desc*="prev"]';
+Pagination.next = '[data-desc*="next"]';
 
 export default Pagination;
